3e164420ee91cc462cd6e22ee8997501dbb701af
Noemie Lachman <noemie.lachman@hotmail.fr>
2020-11-11 18:14:29 +0000

    restaurarnt

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.destroy_all
+
+puts "creating restaurants"
+5.times do
+  new_restaurant = Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    category: ['chinese', 'italian', 'japanese', 'french', 'belgian'].sample
+  )
+
+  puts "created restaurant with id #{new_restaurant.id}"
+end
